07d661d6440d15356d9dda75f11eddcb2c9569c8
Eric Meehan <eric.meehan@icloud.com>
2020-11-22 15:17:03 -0500

    Added the functionality for optional user input

--- Bomb Baby - Python/main.py
+++ Bomb Baby - Python/main.py
@@ -6,11 +6,21 @@ Bomb Baby
 """
 
 from Node import Node
+import sys
 
 def main():
+    # The user may specify a goal position through command line arguments
+    if len(sys.argv) > 1:
+        # Get X from the first argument
+        Position = [int(sys.argv[1])]
+        # Get Y from the second argument
+        Position.append(int(sys.argv[2]))
+    # Alternatively, the default goal of [4, 7] will be used
+    else:
+        Position = [4, 7]
     # Create two instances of Node
     Source = Node([1, 1])
-    Goal = Node([4, 7])
+    Goal = Node(Position)
     # Assume it is possible to reach the goal
     Possible = True
     while Goal != Source:
@@ -18,6 +28,9 @@ def main():
         if Goal < Source:
             Possible = False
             break
-    print(Goal.Operations)
+    if Possible:
+        print(Goal.Operations)
+    else:
+        print("Impossible")
 
 main()
